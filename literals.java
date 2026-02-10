c0b93d2928acb8b3c682b229e01fb767fd19a8cc
sameer <bv014010@gmail.com>
2026-02-11 00:16:14 +0530

    inital commit

---
+++ literals.java
@@ -0,0 +1,13 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age=43;
+        int age2=89;
+        short num=789;
+        long dinoAge=8490394090L;
+        char ch='h';
+        float f1=9.9F;
+        double d1=8.99;
+        System.out.println(age);
+        
+    }
+}

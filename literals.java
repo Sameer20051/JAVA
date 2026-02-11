6a4322c32198a29a9d19c0a0e8da9118b33b311b
sameer <bv014010@gmail.com>
2026-02-12 00:30:32 +0530

    intial commit

--- literals.java
+++ literals.java
@@ -1,13 +1,19 @@
 public class literals {
     public static void main(String[] args) {
         byte age=43;
-        int age2=89;
-        short num=789;
-        long dinoAge=8490394090L;
-        char ch='h';
-        float f1=9.9F;
-        double d1=8.99;
         System.out.println(age);
+        int age2=89;
+        System.out.println(age2);
+        short num=789;
+        System.out.println(num);
+        long dinoAge=8490394090L;
+        System.out.println(dinoAge);
+        char ch='h';
+        System.out.println(ch);
+        float f1=9.9F;
+        System.out.println(f1);
+        double d1=8.99;
+        System.out.println(d1);
         
     }
 }
